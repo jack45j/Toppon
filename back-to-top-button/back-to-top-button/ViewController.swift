32e5cce8132e7d9852c2aa67b3ee6a3ed241d9b3
jack45j <jack45j@gmail.com>
2018-01-17 16:29:57 +0800

    Added popup animation.

--- back-to-top-button/back-to-top-button/ViewController.swift
+++ back-to-top-button/back-to-top-button/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController, TopponDelegate {
-    let toppon = Toppon(initPosition: CGPoint(x:148, y:200),
+    let toppon = Toppon(initPosition: CGPoint(x:80, y:200),
                         size: 48,
                         normalIcon: "top48")
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.toppon.presentMode = .always
-        self.toppon.delegate = self
+        
+        toppon.setPresentMode(.pop)
+        toppon.delegate = self
         view.addSubview(toppon)
     }
     override func viewDidAppear(_ animated: Bool) {
-        toppon.present(toppon, duration: 0.5, delay: 0)
+        toppon.present(toppon)
     }
 }
 
--- back-to-top-button/back-to-top-button/sources/Toppon.swift
+++ back-to-top-button/back-to-top-button/sources/Toppon.swift
@@ -13,22 +13,26 @@ public class Toppon: UIButton {
     /// The TopponDelegate variable, which should be set if you'd like to be notified.
     public weak var delegate: TopponDelegate?
     
-    /// destination position of Topon button.
+    /// Destination position of Topon button.
     /// Will not be use if presentMode isn't Toppon.PresentMode.normal
     public lazy var destPosition: CGPoint? = CGPoint(x:0, y:0)
     
     /// Determines the type of Toppon button present mode.
     /// Default to Toppon.PresentMode.pop
     /// see the presentMode enum for more detail.
-    public lazy var presentMode: PresentMode? = .pop
+    private lazy var presentMode: PresentMode? = .always
     
     /// Determines the type of Toppon button label position.
     /// Default to LabelType.none
     /// See the LabelType enum for more detail.
     public lazy var labelType: LabelType? = .none
     
+    /// Label text for Toppon button.
+    /// Default to LabelType.none
+    /// Set LabelType to others for displaying label text.
     public lazy var labelText: String? = "Top"
     
+    /// Custom label text font style and size.
     public lazy var labelTextFont : UIFont? = UIFont.systemFont(ofSize: 17.0)
     
     /// Initial and return a Toppon object
@@ -51,33 +55,29 @@ public class Toppon: UIButton {
         fatalError("init(coder:) has not been implemented")
     }
     fileprivate func TopponInitial() {
-        self.alpha = 0.0
-        self.addTarget(self, action: #selector(animationPressed(sender:)), for: .touchUpInside)
+        self.addTarget(self, action: #selector(animationPressedScale(sender:)), for: .touchUpInside)
     }
     
-    /// Set Toppon button destination position.
-    /// Will not be used if presentMode set as Toppon.PresentMode.always
-    public func setDestPosition(destPosition: CGPoint) {
-        self.destPosition! = destPosition
-        self.frame.origin = self.destPosition!
-    }
-    public func setPresentMode(presentMode: PresentMode) {
-        self.presentMode! = presentMode
-    }
-    public func setLabelType(labelType: LabelType) {
-        self.labelType! = labelType
+    public func setPresentMode(_ presentMode: PresentMode) {
+        self.presentMode = presentMode
+        if presentMode != .always {
+            alpha = 0.0
+        }
     }
     
     /// Animation for presenting Toppon button.
-    public func present(_ toppon: Toppon, duration: Double, delay: Double) {
-        UIView.animate(withDuration: duration,
-                       delay: delay,
-                       animations: {
-                        toppon.alpha = 1
-                        if toppon.presentMode != .always {
-                            toppon.frame.origin = toppon.destPosition!
-                        }
-            })
+    public func present(_ toppon: Toppon) {
+        switch self.presentMode {
+        case .normal? :
+            TopponLog("Present mode set to normal.")
+        case .pop? :
+            TopponLog("Present mode set to pop.")
+            animationPopUp(sender: toppon)
+        case .always? :
+            TopponLog("Present mode set to always.")
+        default :
+            fatalError("Present mode unknown.")
+        }
     }
 }
 
@@ -88,14 +88,13 @@ public class Toppon: UIButton {
 
 public extension Toppon {
     enum PresentMode {
-        /// Toppon button will move in with animation.
+        /// Toppon button will move in from initPosition to destPosition with animation.
         case normal
         
-        ///Toppon button will popup when app did launch.
+        /// Toppon button will popup when func present(_ toppon: Toppon) being called.
         case pop
         
         /// (DEFAULT) Toppon button will always show.
-        ///
         case always
     }
     
@@ -108,7 +107,26 @@ public extension Toppon {
 }
 
 public extension Toppon {
-    @objc func animationPressed(sender: Toppon) {
+    @objc func animationNormalMoveIn(sender: Toppon) {
+        
+    }
+    @objc func animationNormalMoveOut(sender: Toppon) {
+        
+    }
+    func animationPopUp(sender: Toppon) {
+        sender.transform = CGAffineTransform(scaleX:0.001, y:0.001)
+        sender.alpha = 1.0
+        UIView.animate(withDuration: 0.3,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 0.3,
+                       options: .curveEaseInOut,
+                       animations: {
+                        sender.transform = CGAffineTransform.identity.scaledBy(x: 1.0, y: 1.0)
+        }, completion: nil)
+    }
+    
+    @objc func animationPressedScale(sender: Toppon) {
         delegate?.TopponDidPressed()
         UIView.animate(withDuration: 0.1,
                        delay: 0,
@@ -117,11 +135,8 @@ public extension Toppon {
                         sender.transform = CGAffineTransform(scaleX: 0.85, y: 0.85)
                     }, completion: {(t) in
                         sender.transform = CGAffineTransform(scaleX: 1, y: 1)
-                        self.animationMoveOut(sender: sender)
+                        self.animationNormalMoveOut(sender: sender)
         })
     }
     
-    @objc func animationMoveOut(sender: Toppon) {
-        print("Go")
-    }
 }
