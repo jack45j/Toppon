769461d7988f81e03f82b8dc2a16bba6d588d781
Benson <jack45j@gmail.com>
2020-02-06 01:54:18 +0800

    try to refactor to fluent interface.

--- TopponExample/ViewController.swift
+++ TopponExample/ViewController.swift
@@ -8,53 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController, TopponDelegate, UITextViewDelegate, UIScrollViewDelegate {
-    @IBOutlet weak var scrollview: UITextView!
+	@IBOutlet weak var toppon: Toppon!
+	@IBOutlet weak var scrollview: UITextView!
     @IBOutlet weak var scrollview1: UITextView!
     @IBOutlet weak var scrollview2: UITextView!
     
-    let toppon = Toppon(initPosition: CGPoint(x:320, y:182),
-                        size: 48,
-                        normalIcon: "ScrollToTop.png")
-    let toppon1 = Toppon(initPosition: CGPoint(x:320, y:398),
-                        size: 48,
-                        normalIcon: "ScrollToTopYellow.png")
-    let toppon2 = Toppon(initPosition: CGPoint(x:368, y:612),
-                        size: 48,
-                        normalIcon: "ScrollToBottom.png")
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        toppon.linkedTo(UIScrollView: scrollview)
-        toppon.presentMode = .always
-        view.addSubview(toppon)
-        
-        toppon1.linkedTo(UIScrollView: scrollview1)
-        toppon1.presentMode = .pop
-        view.addSubview(toppon1)
-        
-        toppon2.linkedTo(UIScrollView: scrollview2)
-        toppon2.presentMode = .normal
-        toppon2.scollMode = .bottom
-        toppon2.destPosition = CGPoint(x:320, y:612)
-        view.addSubview(toppon2)
-        
+               
+		toppon.bs
+			.setBackground(color: .yellow)
+			.setTitle("213")
+			.setTitle("333", color: .black, for: .normal)
+			.build()
+		
         scrollview.delegate = self
         scrollview1.delegate = self
         scrollview2.delegate = self
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollview1.contentOffset.y > 30.0 {
-            toppon1.present()
-        } else {
-            toppon1.dismiss()
-        }
-        
-        if scrollview2.contentOffset.y < scrollview2.contentSize.height - scrollview2.bounds.size.height - 30.0 {
-            toppon2.present()
-        } else {
-            toppon2.dismiss()
-        }
+		
     }
 }
 
--- sources/Toppon.swift
+++ sources/Toppon.swift
@@ -8,206 +8,109 @@
 import Foundation
 import UIKit
 
+public struct Builder<T: Toppon> {
+	private let _build: () -> T
+
+	@discardableResult
+	public func build() -> T {
+        _build()
+    }
+
+	public init(_ build: @escaping () -> T) {
+        self._build = build
+    }
+
+    public init(_ base: T) {
+        self._build = { base }
+    }
+}
+
+extension Builder {
+	public func setBackground(color: UIColor) -> Builder<T> {
+		return Builder {
+			let obj = self.build()
+			obj.backgroundColor = color
+			return obj
+		}
+	}
+	
+	public func setBackground(image: UIImage, for state: UIControlState = .normal) -> Builder<T> {
+		return Builder {
+			let obj = self.build()
+			obj.setBackgroundImage(image, for: state)
+			return obj
+		}
+	}
+	
+	public func setTitle(_ title: String?, color: UIColor? = nil, for state: UIControlState = .normal) -> Builder<T> {
+		return Builder {
+			let obj = self.build()
+			obj.setTitle(title, for: state)
+			guard let color = color else { return obj }
+			obj.setTitleColor(color, for: state)
+			return obj
+		}
+	}
+}
+
+public protocol Compatible {}
+extension Compatible where Self: Toppon {
+    public var bs: Builder<Self> {
+		get { Builder(self) }
+    }
+}
+
+
+#if canImport(Foundation)
+import Foundation
+extension Toppon: Compatible {}
+#endif
+
+
 public class Toppon: UIButton {
     /// Determines Toppon is presented or not.
-    fileprivate var isPresented: Bool = false
-    
-    /// Destination position of Topon button.
-    /// Will not be use if presentMode isn't Toppon.PresentMode.normal
-    public lazy var destPosition: CGPoint? = CGPoint(x:0, y:0)
-    public lazy var initPosition: CGPoint? = CGPoint(x:0, y:0)
-    
-    /// Link a UIScrollView or its subclass to Toppon.
-    /// The UIScrollView will scroll to top/bottom after Toppon pressed.
-    public var linkedUIScrollView: UIScrollView?
+	private var isPresented: Bool = false
+	
+	private var CompletionHandler: (() -> Void)? = nil
+	
+	private var linkedScrollViews: [UIScrollView]!
+		
+	private var image: UIImage? = nil
     
     /// Determines the type of Toppon button present mode.
     /// DEFAULT to Toppon.PresentMode.always
     /// See the presentMode enumerated for more detail.
-    public var presentMode: PresentMode = .always {
-        didSet {
-            SetUp()
-        }
-    }
+    public var presentMode: PresentMode = .always
+		
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupUI()
+	}
     
-    /// Determines the type of Toppon button scroll mode.
-    /// DEFAULT to Toppon.ScrollMode.top
-    /// See the ScrollMode enumerated for more detail.
-    public lazy var scollMode: ScrollMode = .top
-    
-    /// The TopponDelegate variable, which should be set if you'd like to be notified.
-    /// See TopponDelegate.swift for more detail.
-    public weak var delegate: TopponDelegate?
-    
-    /// Initial and return a Toppon object
-    /// parameter initPosition: The initial position of Toppon button.
-    /// parameter size:         The width and height for the frame size of Toppon.
-    /// parameter normalIcon:   The image to use for the specified normal state.
-    public init(initPosition: CGPoint?, 
-                size: Int,
-                normalIcon: String?) {
-        let ViewSize = CGSize(width: size, height: size)
-        super.init(frame: CGRect(origin: initPosition!, size: ViewSize))
-        
-        if let icon = normalIcon {
-            setImage(UIImage(named: icon), for: .normal)
-        }
-        
-        self.initPosition = initPosition
-        
-        self.addTarget(self, action: #selector(animationPressedScale(sender:)), for: .touchUpInside)
-        self.addTarget(self, action: #selector(scroll), for: .touchUpInside)
-        
-        delegate?.TopponInitiated()
-    }
-    required public init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    fileprivate func SetUp() {
-        if presentMode != .always {
-            alpha = 0.0
-        }
-    }
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		setupUI()
+	}
+	
+	private func setupUI() {
+		
+	}
 }
 
 /// MARK - Helpers (Config)
 
-extension Toppon {
-    public func linkedTo(UIScrollView: UIScrollView) {
-        self.linkedUIScrollView = UIScrollView
-    }
-}
 
-/// MARK - Helpers (Behavior)
-
-public extension Toppon {
-    public func present() {
-        if !isPresented {
-            delegate?.TopponWillPresent()
-            switch self.presentMode {
-            case .normal :
-                animationNormalMoveIn(sender: self)
-            case .pop :
-                animationPopUp(sender: self)
-            case .always :
-                break
-            }
-            isPresented = true
-        }
-    }
-    
-    public func dismiss() {
-        if isPresented {
-            delegate?.TopponWillDismiss()
-            switch self.presentMode {
-            case .normal :
-                animationNormalMoveOut(sender: self)
-            case .pop :
-                animationPopDown(sender: self)
-            case .always :
-                break
-            }
-            isPresented = false
-        }
-    }
-    
-    @objc fileprivate func scroll() {
-        switch  self.scollMode {
-        case .top:
-            self.linkedUIScrollView!.setContentOffset(.zero, animated: true)
-        case .bottom:
-            let bottomOffset = linkedUIScrollView!.contentSize.height - linkedUIScrollView!.bounds.size.height
-            self.linkedUIScrollView!.setContentOffset(CGPoint(x: 0, y: bottomOffset), animated: true)
-        }
-    }
-}
-
-/// Mark - Enumerated Types (Public)
 
 public extension Toppon {
     enum PresentMode {
         /// (DEFAULT) Toppon button will always show after ViewController launched.
         case always
-        
+
         /// Toppon button will move in from initPosition to destPosition with animation.
         /// Call func present(_ toppon: Toppon) to present Toppon button.
         case normal
-        
+
         /// Toppon button will popup when func present(_ toppon: Toppon) being called.
         case pop
     }
-    
-    enum ScrollMode {
-        /// (DEFAULT) The linked UIScrollView will scroll to top after Toppon pressed.
-        case top
-        
-        /// The linked UIScrollView will scroll to bottom after Toppon pressed.
-        case bottom
-    }
-}
-
-/// MARK - Animations
-
-private extension Toppon {
-    func animationNormalMoveIn(sender: Toppon) {
-        UIView.animate(withDuration: 1.0,
-                       delay: 0.0,
-                       usingSpringWithDamping: 0.5,
-                       initialSpringVelocity: 0.3,
-                       options: .curveEaseOut,
-                       animations: {
-                        sender.frame.origin = self.destPosition!
-                        sender.alpha = 1.0
-        }, completion: nil)
-    }
-    func animationNormalMoveOut(sender: Toppon) {
-        UIView.animate(withDuration: 1.0,
-                       delay: 0.1,
-                       usingSpringWithDamping: 0.5,
-                       initialSpringVelocity: 0.3,
-                       options: .curveEaseIn,
-                       animations: {
-                        sender.frame.origin = self.initPosition!
-                        sender.alpha = 0.0
-        }, completion: nil)
-    }
-    func animationPopUp(sender: Toppon) {
-        sender.transform = CGAffineTransform(scaleX:0.001, y:0.001)
-        sender.alpha = 1.0
-        UIView.animate(withDuration: 0.3,
-                       delay: 0.0,
-                       usingSpringWithDamping: 0.5,
-                       initialSpringVelocity: 0.3,
-                       options: .curveEaseInOut,
-                       animations: {
-                        sender.transform = CGAffineTransform.identity.scaledBy(x: 1.0, y: 1.0)
-        }, completion: nil)
-    }
-    func animationPopDown(sender: Toppon) {
-        sender.transform = CGAffineTransform(scaleX:0.6, y:0.6)
-        UIView.animate(withDuration: 0.3,
-                       delay: 0.1,
-                       usingSpringWithDamping: 0.4,
-                       initialSpringVelocity: 0.3,
-                       options: .curveEaseIn,
-                       animations: {
-                        sender.transform = CGAffineTransform(scaleX:0, y:0)
-                        sender.alpha = 0.0
-        }, completion: nil)
-    }
-    
-    @objc func animationPressedScale(sender: Toppon) {
-        delegate?.TopponDidPressed()
-        UIView.animate(withDuration: 0.1,
-                       delay: 0.0,
-                       options: .autoreverse,
-                       animations: {
-                        sender.transform = CGAffineTransform(scaleX: 0.85, y: 0.85)
-                    }, completion: {(t) in
-                        sender.transform = CGAffineTransform(scaleX: 1, y: 1)
-        })
-    }
-    
 }
